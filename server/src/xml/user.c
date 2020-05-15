a0962eaa3e85abd61462ad2656c518bc6efbaac1
lucas <lucas.fabreguettes@epitech.eu>
2020-05-15 15:53:51 +0200

    feat: Add User function to add user to the xml doc

--- server/src/xml/user.c
+++ server/src/xml/user.c
@@ -8,27 +8,39 @@
 #include <libxml/tree.h>
 #include <time.h>
 #include <uuid/uuid.h>
+#include <string.h>
+#include "logging_server.h"
 
-xmlNodePtr create_user(const char *username, const char *passwd)
+xmlNodePtr user_create(const char *username, const char *passwd)
 {
     xmlNodePtr user;
     uuid_t uuid;
     time_t t = time(NULL);
-    struct tm *tm = localtime(&t);
-    char s[64];
+    struct tm *localt = localtime(&t);
+    char time_str[64];
     char uuid_str[37];
 
-    strftime(s, sizeof(s), "%c", tm);
+    strftime(time_str, sizeof(time_str), "%c", localt);
     uuid_generate((unsigned char *)&uuid);
     uuid_unparse(uuid, uuid_str);
 
     user = xmlNewNode(NULL, BAD_CAST "user");
-
     xmlNewTextChild(user, NULL, BAD_CAST "uuid", BAD_CAST uuid_str);
     xmlNewTextChild(user, NULL, BAD_CAST "username", BAD_CAST username);
     xmlNewTextChild(user, NULL, BAD_CAST "password", BAD_CAST passwd);
-    xmlNewTextChild(user, NULL, BAD_CAST "date", BAD_CAST s);
+    xmlNewTextChild(user, NULL, BAD_CAST "date", BAD_CAST time_str);
     xmlNewTextChild(user, NULL, BAD_CAST "connected", BAD_CAST "false");
-
+    server_event_user_created(uuid_str, username);
     return user;
+}
+
+void user_add(xmlNodePtr user, xmlDocPtr xml_tree)
+{
+    xmlNodePtr root = xmlDocGetRootElement(xml_tree);
+
+    if (!root)
+        return;
+    if (strcmp(root->children->name, "users") != 0)
+        return;
+    xmlAddChild(root->children, user);
 }
