4bb3dc69bc4e480a409571e13f181191c898a169
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-19 18:45:50 +0200

    feat: Change circular buffer to dynamic buffer

--- server/include/buffer.h
+++ server/include/buffer.h
@@ -10,13 +10,11 @@
 
 #include "exception.h"
 
-#define BUFFER_SIZE 2048
+#define BUFFER_SIZE (1024)
 
 typedef struct buffer_s {
-    char buffer[BUFFER_SIZE];
+    char *buffer;
     int buffer_length;
-    int read_index;
-    int write_index;
     exception_t exception;
 } buffer_t;
 
@@ -24,11 +22,9 @@ typedef struct buffer_s {
 buffer_t buffer_create(void);
 
 /*  buffer_read.c  */
-char buffer_read_character(buffer_t *buffer);
 char *buffer_read_string(buffer_t *buffer);
 
 /*  buffer_write.c  */
-void buffer_write_character(buffer_t *buffer, const char c);
-void buffer_write_string(buffer_t *buffer, const char *const str);
+void buffer_write_string(buffer_t *buffer, const char *str);
 
 #endif  // NWP_MYTEAMS_2019_BUFFER_H
