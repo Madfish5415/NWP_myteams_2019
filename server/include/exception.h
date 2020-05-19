0014585b07478c517695bd6a1373ed1384713d2a
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-19 18:46:32 +0200

    fix: Delete 'BAD_CAST' exception due to libxml2 macro

--- server/include/exception.h
+++ server/include/exception.h
@@ -15,7 +15,6 @@
 enum error_type {
     NO_ERROR = 0,
     BAD_ALLOC,
-    BAD_CAST,
     BAD_TYPEID,
     BAD_EXCEPTION,
     LOGIC_FAILURE,
@@ -42,7 +41,7 @@ typedef struct exception_s {
 } exception_t;
 
 exception_t new_exception(int16_t code, const char *where, const char *what);
-bool catch (exception_t exception);
+bool catch(exception_t exception);
 void print_exception(exception_t exception);
 bool catch_and_print(exception_t exception);
 
