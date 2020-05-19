748d354b2bf1549e57a24b80066f7e89f9762f13
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-19 18:54:25 +0200

    feat: Add client executing

--- server/include/client.h
+++ server/include/client.h
@@ -9,8 +9,10 @@
 #define NWP_MYTEAMS_2019_CLIENT_H
 
 #include <uuid/uuid.h>
+#include <netinet/in.h>
 
 #include "buffer.h"
+#include "def.h"
 
 enum use_type {
     NONE = 0,
@@ -20,11 +22,32 @@ enum use_type {
 };
 
 typedef struct client_s {
-    uuid_t user;
+    socket_t socket;
+    struct sockaddr_in address;
+    size_t address_length;
+    char user[UUID_SIZE];
     buffer_t read_queue;
-    char *write_queue;
+    buffer_t write_queue;
     int use_type;
-    uuid_t use_uuid;
+    char use_uuid[UUID_SIZE];
 } client_t;
+
+typedef struct server_s server_t;  // Forward declare server_t
+
+/*  client_connect.c  */
+void client_connect(server_t *server);
+
+/*  client_disconnect.c  */
+void client_disconnect(server_t *server, client_t *client);
+
+/*  client_execute.c  */
+void client_execute(server_t *server, client_t *client);
+
+/*  client_read.c  */
+void client_read(server_t *server, client_t *client);
+
+/*  client_write.c  */
+void client_write(server_t *server, client_t *client);
+
 
 #endif  // NWP_MYTEAMS_2019_CLIENT_H
