0330172f57c19fcecd9f4676f23af8ed19d582d7
Matteo Fauchon <matteo.fauchon@epitech.eu>
2020-05-20 12:21:10 +0200

    feat: handle the SIGINT signal
    fix: useless usleep

--- client/src/client/client_loop.c
+++ client/src/client/client_loop.c
@@ -11,16 +11,35 @@
 
 #include "client.h"
 #include "help.h"
+#include <signal.h>
+
+static bool run(bool stop, client_t client)
+{
+    static bool is_running = true;
+
+    if (stop) {
+        send(client.sock, "/logout\n", strlen("/logout\n"), 0);
+        is_running = false;
+    }
+    return is_running;
+}
+
+static void signal_handler(int sig)
+{
+    (void) sig;
+
+    run(true);
+}
 
 void client_loop(client_t client)
 {
-    while (1) {
+    signal(SIGINT, signal_handler);
+    while (run(false, client)) {
         read(STDIN_FILENO, client.printer, sizeof(client.printer));
         if (strcmp(client.printer, "/help\n") == 0) {
             printf("%s\n", HELP_STRING);
         } else {
             send(client.sock, client.printer, strlen(client.printer), 0);
-            usleep(2000);
         }
         client_read_server(&client);
         if (client.reader) {
