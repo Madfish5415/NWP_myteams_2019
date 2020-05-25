7df40e660a6f4d69374ebf2afe9903081965aee1
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-25 17:24:05 +0200

    feat: Add info commands

---
+++ server/src/commands/info/info_channel.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** NWP_myteams_2019
+** File description:
+** info_channel.c
+*/
+
+#include "server.h"
+
+void info_channel(server_t *server, client_t *client, char **cmds)
+{
+
+}
---
+++ server/src/commands/info/info_team.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** NWP_myteams_2019
+** File description:
+** info_team.c
+*/
+
+#include "server.h"
+
+void info_team(server_t *server, client_t *client, char **cmds)
+{
+
+}
