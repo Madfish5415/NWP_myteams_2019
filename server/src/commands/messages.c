f58b589712b3acd66d3dc1bb4ff74336d8b32758
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-25 17:22:26 +0200

    feat: Add messages command

--- server/src/commands/messages.c
+++ server/src/commands/messages.c
@@ -7,7 +7,40 @@
 
 #include "server.h"
 
+static void send_all_messages(server_t *server, client_t *client, char **cmds)
+{
+    xmlNodePtr discussion =
+        discussion_get(server->xml_tree, client->user, cmds[1]);
+
+    if (!discussion) {
+        return;
+    }
+    if (discussion->children->next->next->children)
+        server_send_response(server, client, RESPONSE_255, false);
+    for (xmlNodePtr message = discussion->children->next->next->children;
+        message; message = message->next) {
+        server_send_response(
+            server, client, (char *)xmlNodeGetContent(message->children), true);
+        server_send_response(server, client,
+            (char *)xmlNodeGetContent(message->children->next), true);
+        server_send_response(server, client,
+            (char *)xmlNodeGetContent(message->children->next->next), true);
+    }
+}
+
 void cmd_messages(server_t *server, client_t *client, char **cmds)
 {
-    if (!user_get_authorize(server, client, cmds)) return;
+    if (!user_get_authorize(server, client, cmds))
+        return;
+
+    if (cmds[1] == NULL) {
+        server_send_response(server, client, RESPONSE_501, true);
+        return;
+    }
+    if (!user_get_by_uuid(server->xml_tree, cmds[1])) {
+        server_send_response(server, client, RESPONSE_501, false);
+        server_send_response(server, client, cmds[1], true);
+        return;
+    }
+    send_all_messages(server, client, cmds);
 }
