ced4d7f938cea5914dd97261a7437798af3482d9
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-25 17:23:10 +0200

    feat: Add user command

--- server/src/commands/user.c
+++ server/src/commands/user.c
@@ -5,9 +5,39 @@
 ** user.c
 */
 
+#include <string.h>
+
 #include "server.h"
+
+static void print_user(server_t *server, client_t *client, xmlNodePtr user)
+{
+    server_send_response(server, client, RESPONSE_242, false);
+    server_send_response(
+        server, client, (char *)xmlNodeGetContent(user->children), true);
+    server_send_response(
+        server, client, (char *)xmlNodeGetContent(user->children->next), true);
+    server_send_response(server, client,
+        ((strcmp(
+        (char *)xmlNodeGetContent(user->children->next->next->next->next),
+        "false") == 0) ? "0" : "1"), true);
+}
 
 void cmd_user(server_t *server, client_t *client, char **cmds)
 {
-    if (!user_get_authorize(server, client, cmds)) return;
+    xmlNodePtr user = NULL;
+
+    if (!user_get_authorize(server, client, cmds))
+        return;
+
+    if (cmds[1] == NULL) {
+        server_send_response(server, client, RESPONSE_501, true);
+        return;
+    }
+    user = user_get_by_uuid(server->xml_tree, cmds[1]);
+    if (!user) {
+        server_send_response(server, client, RESPONSE_501, false);
+        server_send_response(server, client, cmds[1], true);
+    } else {
+        print_user(server, client, user);
+    }
 }
