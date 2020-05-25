8482466951b5a247500dbc762fdc9d7695aa3c7a
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-25 17:17:12 +0200

    feat: Delete buffer properly

---
+++ server/src/buffer/buffer_delete.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** NWP_myteams_2019
+** File description:
+** buffer_delete.c
+*/
+
+#include "buffer.h"
+
+void buffer_delete(buffer_t *buffer)
+{
+    if (buffer->buffer)
+        free(buffer->buffer);
+    buffer->buffer_length = 0;
+    catch_and_print(buffer->exception);
+}
