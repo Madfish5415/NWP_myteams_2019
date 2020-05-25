0e3c19cf0adf6e21e6cf56eaadf97d8ab307f3b5
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-25 17:24:11 +0200

    feat: Add list commands

---
+++ server/src/commands/list/list_thread.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** NWP_myteams_2019
+** File description:
+** list_thread.c
+*/
+
+#include "server.h"
+
+void list_thread(server_t *server, client_t *client, char **cmds)
+{
+
+}
