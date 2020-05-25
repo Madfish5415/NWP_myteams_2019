3d1133fc262f482229216badda4d76c6001e6ffb
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-25 17:21:45 +0200

    feat: Add create distributor

--- server/src/commands/create.c
+++ server/src/commands/create.c
@@ -6,8 +6,14 @@
 */
 
 #include "server.h"
+#include "use.h"
 
 void cmd_create(server_t *server, client_t *client, char **cmds)
 {
     if (!user_get_authorize(server, client, cmds)) return;
+
+    for (int i = 0; CREATE_MAPPING[i].callback != NULL; i++) {
+        if (CREATE_MAPPING[i].use_type == client->use_type)
+            return CREATE_MAPPING[i].callback(server, client, cmds);
+    }
 }
