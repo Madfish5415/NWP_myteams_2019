b8644533a2fcc4e2c0a87a2827231dfa6bdfa017
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-25 17:21:51 +0200

    feat: Add info distributor

--- server/src/commands/info.c
+++ server/src/commands/info.c
@@ -6,8 +6,14 @@
 */
 
 #include "server.h"
+#include "use.h"
 
 void cmd_info(server_t *server, client_t *client, char **cmds)
 {
     if (!user_get_authorize(server, client, cmds)) return;
+
+    for (int i = 0; INFO_MAPPING[i].callback != NULL; i++) {
+        if (INFO_MAPPING[i].use_type == client->use_type)
+            return INFO_MAPPING[i].callback(server, client, cmds);
+    }
 }
