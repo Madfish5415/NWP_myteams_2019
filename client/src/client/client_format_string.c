0ac798d53b05d7b96247e44118f2a601005d22e8
Matteo Fauchon <matteo.fauchon@epitech.eu>
2020-05-25 15:50:33 +0200

    fix: send formated string to the server, with "\r\n" in delimiter

--- client/src/client/client_format_string.c
+++ client/src/client/client_format_string.c
@@ -8,25 +8,56 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "utils.h"
+static char *find_string(char *string)
+{
+    static unsigned int idx = 0;
+    unsigned int size = 0;
+    unsigned int i = 0;
+    char *tmp = NULL;
+
+    for (; string[idx] && string[idx] != '"'; idx++)
+        ;
+    idx++;
+    for (size = idx; string[size] && string[size] != '"'; size++)
+        ;
+    tmp = malloc(sizeof(char) * (size - idx + 1));
+    for (i = 0; string[idx] && string[idx] != '"'; idx++, i++)
+        tmp[i] = string[idx];
+    idx++;
+    tmp[i] = '\0';
+    return tmp;
+}
+
+static unsigned int get_args(char *string)
+{
+    unsigned long nb_quotes = 0;
+
+    for (int i = 0; string[i]; i++)
+        nb_quotes += (string[i] == '\"');
+    nb_quotes = (nb_quotes / 2) + 1;
+    return nb_quotes;
+}
 
 char *format_string(char *string)
 {
-    char **tab = NULL;
+    char *cmd = NULL;
     char *format = NULL;
-    unsigned long len = 0;
+    char *tmp = NULL;
 
-    tab = split(strdup(string), " ");
-    if (!tab)
-        return NULL;
-    for (int i = 0; tab[i] != NULL; i++) {
-        len = ((format) ? strlen(format) : 0);
-        format = realloc(format, sizeof(char) * (len + strlen(tab[i]) + 4));
-        if (!format)
-            return NULL;
-        format = strcat(format, tab[i]);
+    if (string[0] == 0) return NULL;
+    tmp = strdup(string);
+    cmd = strtok(tmp, " ");
+    if (!cmd) return NULL;
+    format = strdup(cmd);
+    for (unsigned int i = 1; i < get_args(string); i++) {
+        tmp = find_string(string);
+        format =
+            realloc(format, sizeof(char) * (strlen(format) + strlen(tmp) + 3));
         format = strcat(format, "\r\n\0");
-        format[len + strlen(tab[i]) + 4] = '\0';
+        format = strcat(format, tmp);
     }
+    free(cmd);
+    free(tmp);
+    format = strcat(format, "\r\n\0");
     return format;
 }
