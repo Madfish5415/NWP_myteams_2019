ef8955023bfbfed022052f5f158fca556e0fe8d7
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-25 17:23:44 +0200

    feat: Add users command

--- server/src/commands/users.c
+++ server/src/commands/users.c
@@ -5,9 +5,30 @@
 ** users.c
 */
 
+#include <string.h>
+
 #include "server.h"
 
 void cmd_users(server_t *server, client_t *client, char **cmds)
 {
-    if (!user_get_authorize(server, client, cmds)) return;
+    xmlNodePtr root = NULL;
+
+    if (!user_get_authorize(server, client, cmds))
+        return;
+    root = xmlDocGetRootElement(server->xml_tree);
+    if (!root || !root->children ||
+        strcmp((char *)root->children->name, "users") != 0)
+        return;
+    if (root->children->children)
+        server_send_response(server, client, RESPONSE_240, false);
+    for (xmlNodePtr user = root->children->children; user; user = user->next) {
+        server_send_response(
+            server, client, (char *)xmlNodeGetContent(user->children), true);
+        server_send_response(server, client,
+            (char *)xmlNodeGetContent(user->children->next), true);
+        server_send_response(server, client,
+             ((strcmp((char *)xmlNodeGetContent(
+             user->children->next->next->next->next), "false") == 0) ?
+             "0" : "1"),true);
+    }
 }
