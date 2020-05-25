f93e91c1775e72e6c28b885cb5f847c81dbd305e
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-25 17:19:09 +0200

    fix: Fix discussion methods

--- server/src/xml/discussion.c
+++ server/src/xml/discussion.c
@@ -18,36 +18,42 @@
 xmlNodePtr discussion_create(const char *user_id, const char *user_id2)
 {
     xmlNodePtr discussion;
+    xmlNodePtr messages;
 
     discussion = xmlNewNode(NULL, BAD_CAST "discussion");
     xmlNewTextChild(discussion, NULL, BAD_CAST "uuid", BAD_CAST user_id);
     xmlNewTextChild(discussion, NULL, BAD_CAST "uuid", BAD_CAST user_id2);
+    messages = xmlNewNode(NULL, BAD_CAST "messages");
+    xmlAddChild(discussion, messages);
     return discussion;
 }
 
-bool check_ids(xmlNodePtr discussion, const char *user_id, const char *user_id2)
+static bool check_ids(
+    xmlNodePtr discussion, const char *user_id, const char *user_id2)
 {
-    if (strcmp((char *)discussion->name, user_id) == 0 &&
-        strcmp((char *)discussion->next->name, user_id2) == 0)
+    if (strcmp((char *)xmlNodeGetContent(discussion), user_id) == 0 &&
+        strcmp((char *)xmlNodeGetContent(discussion->next), user_id2) == 0)
         return true;
-    if (strcmp((char *)discussion->name, user_id2) == 0 &&
-        strcmp((char *)discussion->next->name, user_id) == 0)
+    if (strcmp((char *)xmlNodeGetContent(discussion), user_id2) == 0 &&
+        strcmp((char *)xmlNodeGetContent(discussion->next), user_id) == 0)
         return true;
     return false;
 }
 
-void add_discussion(xmlNodePtr discussions, const char *user_id,
+static void add_discussion(xmlNodePtr discussions, const char *user_id,
     const char *user_id2, xmlNodePtr message)
 {
     for (xmlNodePtr discussion = discussions->children; discussion;
-            discussion = discussion->next) {
-        if (!discussion->children) return;
+        discussion = discussion->next) {
+        if (!discussion->children)
+            return;
         if (check_ids(discussion->children, user_id, user_id2)) {
             xmlAddChild(discussion->children->next->next, message);
             return;
         }
     }
     xmlAddChild(discussions, discussion_create(user_id, user_id2));
+    add_discussion(discussions, user_id, user_id2, message);
 }
 
 exception_t discussion_add_message(xmlDocPtr xml_tree, const char *user_id,
@@ -61,11 +67,30 @@ exception_t discussion_add_message(xmlDocPtr xml_tree, const char *user_id,
             "discussion_add (xml/discussion.c)", "Root of the file not found");
         return exception;
     }
-    if (!root->children || root->children->next || root->children->next->next) {
+    if (!root->children || !root->children->next ||
+        !root->children->next->next) {
         exception = new_exception(OUT_OF_RANGE,
             "discussion_add (xml/discussion.c)", "Discussions not found");
         return exception;
     }
     add_discussion(root->children->next->next, user_id, user_id2, message);
     return exception;
+}
+
+xmlNodePtr discussion_get(
+    xmlDocPtr xml_tree, const char *user_id, const char *user_id2)
+{
+    xmlNodePtr root = xmlDocGetRootElement(xml_tree);
+
+    if (!root || !root->children || !root->children->next ||
+        !root->children->next->next ||
+        strcmp((char *)root->children->next->next->name, "discussions") != 0)
+        return NULL;
+    for (xmlNodePtr discussion = root->children->next->next->children;
+        discussion; discussion = discussion->next) {
+        if (check_ids(discussion->children, user_id, user_id2)) {
+            return discussion;
+        }
+    }
+    return NULL;
 }
