3227e35c00650ece13fb85160c8dbc022afd7101
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-25 17:22:11 +0200

    feat: Add login command

--- server/src/commands/login.c
+++ server/src/commands/login.c
@@ -5,9 +5,64 @@
 ** login.c
 */
 
+#include <string.h>
+
+#include "logging_server.h"
 #include "server.h"
+#include "cmd.h"
+
+static void succesfully_connected(
+    server_t *server, client_t *client, xmlNodePtr user)
+{
+    server_broadcast(server, RESPONSE_230, false);
+    server_broadcast(server, (char *)xmlNodeGetContent(user->children), true);
+    server_broadcast(
+        server, (char *)xmlNodeGetContent(user->children->next), true);
+    server_event_user_logged_in((char *)xmlNodeGetContent(user->children));
+    for (int i = 0; i < UUID_SIZE &&
+        ((char *)xmlNodeGetContent(user->children))[i] != '\0'; i++)
+        client->user[i] = ((char *)xmlNodeGetContent(user->children))[i];
+    xmlNodeSetContent(user->children->next->next->next->next, BAD_CAST "true");
+}
+
+static void create_new_user(server_t *server, xmlNodePtr *user, char **cmds)
+{
+    (*user) = user_create(cmds[1], "");
+    user_add((*user), server->xml_tree);
+    xmlNodeSetContent(
+        (*user)->children->next->next->next->next, BAD_CAST "true");
+}
+
+bool is_already_connected(server_t *server, client_t *client, xmlNodePtr user)
+{
+    if (strcmp(
+            (char *)xmlNodeGetContent(user->children->next->next->next->next),
+            "true") == 0) {
+        server_send_response(server, client, RESPONSE_506, false);
+        return true;
+    }
+    return false;
+}
 
 void cmd_login(server_t *server, client_t *client, char **cmds)
 {
-    if (!user_get_authorize(server, client, cmds)) return;
+    xmlNodePtr user = NULL;
+
+    if (!user_get_authorize(server, client, cmds))
+        return;
+
+    if (cmds[1] == NULL) {
+        server_send_response(server, client, RESPONSE_506, false);
+        return;
+    }
+    user = user_get_by_name(server->xml_tree, cmds[1]);
+    if (user == NULL) {
+        create_new_user(server, &user, cmds);
+    } else {
+        if (is_already_connected(server, client, user))
+            return;
+    }
+    if (client->user[0] != '\0')
+        cmd_logout(server, client, cmds);
+    succesfully_connected(server, client, user);
 }
