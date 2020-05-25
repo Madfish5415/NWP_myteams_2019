ea87adfe7db57fe19c01c17ac454dd6f8530815c
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-25 17:22:44 +0200

    feat: Add subscribe command

--- server/src/commands/subscribe.c
+++ server/src/commands/subscribe.c
@@ -6,8 +6,36 @@
 */
 
 #include "server.h"
+#include "logging_server.h"
+
+static void subscribe_to_team(server_t *server, client_t *client, char **cmds)
+{
+    xmlNodePtr subscribe = subscribe_create(client->user);
+    subscribe_add(subscribe, server->xml_tree, cmds[1]);
+    server_event_user_join_a_team(cmds[1], client->user);
+    server_send_response(server, client, RESPONSE_250, false);
+    server_send_response(server, client, client->user, true);
+    server_send_response(server, client, cmds[1], true);
+}
 
 void cmd_subscribe(server_t *server, client_t *client, char **cmds)
 {
+    xmlNodePtr team = NULL;
+
     if (!user_get_authorize(server, client, cmds)) return;
+    if (cmds[1] == NULL) {
+        server_send_response(server, client, RESPONSE_502, true);
+        return;
+    }
+    team = team_get(server->xml_tree, cmds[1]);
+    if (!team) {
+        server_send_response(server, client, RESPONSE_502, false);
+        server_send_response(server, client, cmds[1], true);
+        return;
+    }
+    if (subscriber_get(server->xml_tree, cmds[1], client->user)) {
+        server_send_response(server, client, RESPONSE_506, true);
+        return;
+    }
+    subscribe_to_team(server, client, cmds);
 }
