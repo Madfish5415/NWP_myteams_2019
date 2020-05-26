a9b8a7080fc9c891b9e2bbf6f9cf7c1a1b80ac28
lucas <lucas.fabreguettes@epitech.eu>
2020-05-26 15:29:19 +0200

    feat: Add info commands

--- server/src/commands/info/info_channel.c
+++ server/src/commands/info/info_channel.c
@@ -9,5 +9,16 @@
 
 void info_channel(server_t *server, client_t *client, char **cmds)
 {
+    xmlNodePtr channel = channel_get(server->xml_tree, client->use_uuid);
 
+    (void)cmds;
+    if (!channel)
+        return;
+    server_send_response(server, client, RESPONSE_244, false);
+    server_send_response(
+        server, client, (char *)xmlNodeGetContent(channel->children), true);
+    server_send_response(server, client,
+        (char *)xmlNodeGetContent(channel->children->next), true);
+    server_send_response(server, client,
+        (char *)xmlNodeGetContent(channel->children->next->next), true);
 }
--- server/src/commands/info/info_team.c
+++ server/src/commands/info/info_team.c
@@ -9,5 +9,16 @@
 
 void info_team(server_t *server, client_t *client, char **cmds)
 {
+    xmlNodePtr team = team_get(server->xml_tree, client->use_uuid);
 
+    (void) cmds;
+    if (!team)
+        return;
+    server_send_response(server, client, RESPONSE_243, false);
+    server_send_response(
+        server, client, (char *)xmlNodeGetContent(team->children), true);
+    server_send_response(
+        server, client, (char *)xmlNodeGetContent(team->children->next), true);
+    server_send_response(server, client,
+        (char *)xmlNodeGetContent(team->children->next->next), true);
 }
