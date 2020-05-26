6193828f5e2f7b57786a7d86ace3eec692484e35
lucas <lucas.fabreguettes@epitech.eu>
2020-05-26 15:55:31 +0200

    feat: List command

--- server/src/commands/list/list_channel.c
+++ server/src/commands/list/list_channel.c
@@ -9,5 +9,25 @@
 
 void list_channel(server_t *server, client_t *client, char **cmds)
 {
+    xmlNodePtr channel = channel_get(server->xml_tree, client->use_uuid);
 
+    if (!channel)
+        return;
+    server_send_response(server, client, RESPONSE_253, false);
+    for (xmlNodePtr thread =
+             channel->children->next->next->next->next->next->children;
+         thread; thread = thread->next) {
+        server_send_response(
+            server, client, (char *)xmlNodeGetContent(thread->children), true);
+        server_send_response(server, client,
+            (char *)xmlNodeGetContent(thread->children->next->next->next->next),
+            true);
+        server_send_response(server, client,
+            (char *)xmlNodeGetContent(thread->children->next->next->next),
+            true);
+        server_send_response(server, client,
+            (char *)xmlNodeGetContent(thread->children->next), true);
+        server_send_response(server, client,
+            (char *)xmlNodeGetContent(thread->children->next->next), true);
+    }
 }
--- server/src/commands/list/list_nocontext.c
+++ server/src/commands/list/list_nocontext.c
@@ -9,5 +9,18 @@
 
 void list_nocontext(server_t *server, client_t *client, char **cmds)
 {
+    xmlNodePtr root = xmlDocGetRootElement(server->xml_tree);
+    xmlNodePtr team = root->children->next->children;
 
+    if (!team)
+        return;
+    server_send_response(server, client, RESPONSE_241, false);
+    for (; team; team = team->next) {
+        server_send_response(
+            server, client, (char *)xmlNodeGetContent(team->children), true);
+        server_send_response(server, client,
+            (char *)xmlNodeGetContent(team->children->next), true);
+        server_send_response(server, client,
+            (char *)xmlNodeGetContent(team->children->next->next), true);
+    }
 }
