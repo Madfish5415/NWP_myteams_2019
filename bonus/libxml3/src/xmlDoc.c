06d2f22d17599112d2f5630a314a7f506bbddd60
lucas <lucas.fabreguettes@epitech.eu>
2020-05-28 14:45:54 +0200

    feat: Add Root functions for libxml3

--- bonus/libxml3/src/xmlDoc.c
+++ bonus/libxml3/src/xmlDoc.c
@@ -4,3 +4,23 @@
 ** File description:
 ** xmlDoc.c
 */
+
+#include <stdlib.h>
+
+#include "libxml3.h"
+
+xmlNodePtr xmlDocGetRootElement(xmlDocPtr doc)
+{
+    if (!doc)
+        return NULL;
+    return doc->children;
+}
+
+xmlNodePtr xmlDocSetRootElement(xmlDocPtr doc, xmlNodePtr root)
+{
+    if (!doc || !root)
+        return NULL;
+    if (doc->children)
+        free(doc->children);
+    doc->children = root;
+}
