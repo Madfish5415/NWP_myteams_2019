c61d0f062f0189bdd512c48d933190b08b695570
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-29 12:11:04 +0200

    feat: Add xmlFree method

--- bonus/libxml3/src/xml_free.c
+++ bonus/libxml3/src/xml_free.c
@@ -14,8 +14,22 @@ void xmlFreeNode(xmlNodePtr cur)
     if (!cur)
         return;
     if (cur->children) {
-        for (xmlNodePtr tmp = cur->children; tmp; tmp = tmp->next)
-            xmlFreeNode(tmp);
+        xmlFreeNode(cur->children);
     }
+    if (cur->next) {
+        xmlFreeNode(cur->next);
+    }
+    if (cur->name) free(cur->name);
+    if (cur->content) free(cur->content);
     free(cur);
+}
+
+void xmlFreeDoc(xmlDocPtr doc)
+{
+    if (!doc) return;
+    if (doc->name) free(doc->name);
+    if (doc->encoding) free(doc->encoding);
+    if (doc->version) free(doc->version);
+    if (doc->children) xmlFreeNode(doc->children);
+    free(doc);
 }
