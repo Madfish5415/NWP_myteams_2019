47fec865c28994cb1b46a49799b59106202e4f70
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-29 12:48:33 +0200

    fix: List objects backwards

--- server/src/commands/list/list_team.c
+++ server/src/commands/list/list_team.c
@@ -16,8 +16,8 @@ void list_team(server_t *server, client_t *client, char **cmds)
         return;
     server_send_response(server, client, RESPONSE_252, false);
     for (xmlNodePtr channel =
-             team->children->next->next->next->next->next->children;
-         channel; channel = channel->next) {
+        team->children->next->next->next->next->next->last;
+        channel; channel = channel->prev) {
         server_send_response(
             server, client, (char *)xmlNodeGetContent(channel->children), true);
         server_send_response(server, client,
