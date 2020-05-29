d3c0486cbe54b5c649baff34d9575f845db5015d
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-29 12:10:58 +0200

    feat: Add xmlUnlink method

--- bonus/libxml3/src/xml_unlink.c
+++ bonus/libxml3/src/xml_unlink.c
@@ -4,3 +4,23 @@
 ** File description:
 ** xmlUnlink.c
 */
+
+#include <stdlib.h>
+
+#include "libxml3.h"
+
+void xmlUnlinkNode(xmlNodePtr cur)
+{
+    if (cur == NULL)
+        return;
+
+    if (cur->prev) cur->prev->next = cur->next;
+    if (cur->next) cur->next->prev = cur->prev;
+    if (cur->parent) {
+        if (cur->parent->last == cur) cur->parent->last = cur->prev;
+        if (cur->parent->children == cur) cur->parent->children = cur->next;
+    }
+    cur->prev = NULL;
+    cur->next = NULL;
+    cur->parent = NULL;
+}
