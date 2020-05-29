2ffe601a099091c1c8a076ad62da5a89b0cfa33a
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-05-29 12:10:35 +0200

    feat: Add xmlParse method

--- bonus/libxml3/src/xml_parse.c
+++ bonus/libxml3/src/xml_parse.c
@@ -5,16 +5,48 @@
 ** xmlParse.c
 */
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "libxml3.h"
+#include "parse.h"
+
+char *readFile(const char *filename)
+{
+    FILE *file;
+    char *line = NULL;
+    char *result = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    file = fopen(filename, "r");
+    if (file == NULL)
+        return NULL;
+    while ((read = getline(&line, &len, file)) != -1) {
+        if (!result) {
+            result = malloc(sizeof(char) * len);
+            memset(result, '\0', len);
+        } else
+            result = realloc(result, strlen(result) + len);
+        strcat(result, line);
+    }
+    if (line) free(line);
+    fclose(file);
+    return result;
+}
 
 xmlDocPtr xmlParseFile(const char *filename)
 {
     xmlDocPtr doc = NULL;
+    char *content = NULL;
 
-    if (filename == NULL)
-        return NULL;
-
+    if (filename == NULL) return NULL;
+    content = readFile(filename);
+    if (content == NULL) return NULL;
+    if ((doc = xmlNewDoc(NULL)) == NULL) return NULL;
+    setParams(content, doc);
+    createTree(getRootElem(content), doc);
+    free(content);
     return doc;
 }
